b4fff160f5d64e46eff8ce476443db979aea11bc
mukeshdroid <mukeshtiwari200102@gmail.com>
2024-03-31 00:57:57 +0545

    added sexratio poverty and others to data

--- dashboard_app/app.py
+++ dashboard_app/app.py
@@ -5,9 +5,6 @@ import altair as alt
 import plotly.express as px
 import geopandas as gpd
 
-#
-df_geometry = gpd.read_file("data/nepal-districts_filtered.geojson")
-
 # configure page
 st.set_page_config(
     page_title="Nepal Population Dashboard",
@@ -19,7 +16,73 @@ st.set_page_config(
 alt.themes.enable("dark")
 
 # dataframe
-df_nepal = pd.read_csv("data/districts_complete_normalized.csv")
+df_nepal = pd.read_csv("data/districts_complete.csv")
+df_province = pd.read_csv("data/province_complete.csv")
+#load nepal's geometry divided at district level
+df_geometry = gpd.read_file("data/nepal-districts_filtered.geojson")
+
+
+#------------------------------DEFINE FUNCTIONS-------------------------------
+# define cholropleth map
+def make_choropleth(input_df, input_id, input_column, input_color_theme):
+    choropleth = px.choropleth(
+        input_df,
+        geojson=df_geometry,
+        featureidkey="properties.DIST_EN",
+        locations=input_id,
+        color=input_column,
+        color_continuous_scale=input_color_theme,
+        # range_color=(0, max(df_nepal.number)),
+        labels={"population": "Population"},
+    )
+    choropleth.update_layout(
+        template="plotly_dark",
+        plot_bgcolor="rgba(0, 0, 0, 0)",
+        paper_bgcolor="rgba(0, 0, 0, 0)",
+        margin=dict(l=0, r=0, t=0, b=0),
+        height=350,
+    )
+    return choropleth.update_geos(fitbounds="locations", visible=True)
+
+
+
+# radio buttons for choropleth type
+def choropleth_radio():
+    info_on = st.radio(
+            "Paramter to color map by:",
+            ["Population", "Sex Ratio", "Poverty", "Literacy", "Student-Teacher Ratio","Population Density"],
+        )
+    return info_on
+
+
+#top 6 lowest and highest population 2021
+def compute_top5population(df):
+    df_top5population = df.sort_values(['PopulationCensus2021-11-25'],ascending=False).head(6)[["Name","PopulationCensus2021-11-25"]].rename(columns={'PopulationCensus2021-11-25': 'Population'})
+    return df_top5population
+
+#top 6 lowest and highest population 2021
+def compute_low5population(df):
+    df_low5population = df.sort_values(['PopulationCensus2021-11-25'],ascending=True).head(6)[["Name","PopulationCensus2021-11-25"]].rename(columns={'PopulationCensus2021-11-25': 'Population'})
+    return df_low5population
+
+#generate line chart to show the increase of population by province
+def genlinechart_province(df):
+
+    df_temp = df[["Name", "Population1981","Population1991","Population2001","Population2011","Population2021"]]
+    df_temp = df_temp.rename(columns={"Population1981" : 1981,"Population1991" : 1991,"Population2001" : 2001,"Population2011" : 2011,"Population2021":2021})
+    return df_temp.set_index('Name').T
+
+#generate pie chart to show contribution of population by province
+def genpiechart_province(df):
+        fig = px.pie(df, values='Population2021', names='Name',
+                 title=f'number of ',
+                 height=300, width=200)
+        fig.update_layout(margin=dict(l=20, r=20, t=30, b=0),)
+        return fig
+
+
+
+#------------------------------------LAYOUT---------------------------------------------
 
 with st.sidebar:
 
@@ -50,62 +113,21 @@ with st.sidebar:
             "Select District", province_district_dict[select_province]
         )
 
-        # radio buttons for info type
-        info_on = st.radio(
-            "What do you want to see information about?",
-            ["Population", "Literacy Rate", "Sex Raio", "Poverty"],
-        )
-
+        choropleth_radio()
         st.form_submit_button("Show Info")
 
         #info section
-        st.info('Built by [Priyanka](https://www.google.com) and [Mukesh](https://www.google.com) \n Checkout code at [Github Repo](https://www.google.com)', icon="ℹ️")
-
-# define cholropleth map
-def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    choropleth = px.choropleth(
-        input_df,
-        geojson=df_geometry,
-        featureidkey="properties.DIST_EN",
-        locations=input_id,
-        color=input_column,
-        color_continuous_scale=input_color_theme,
-        # range_color=(0, max(df_nepal.number)),
-        labels={"population": "Population"},
-    )
-    choropleth.update_layout(
-        template="plotly_dark",
-        plot_bgcolor="rgba(0, 0, 0, 0)",
-        paper_bgcolor="rgba(0, 0, 0, 0)",
-        margin=dict(l=0, r=0, t=0, b=0),
-        height=350,
-    )
-    return choropleth.update_geos(fitbounds="locations", visible=True)
+        st.info('Built by [Priyanka](https://www.linkedin.com/in/priyanka-panta-8451b4251/) and [Mukesh](https://www.linkedin.com/in/tiwarimukesh12/) \n Checkout code at [Github Repo](https://www.google.com)', icon="ℹ️")
 
 
 
 choropleth = make_choropleth(df_nepal, "Name", "PopulationCensus2021-11-25", "Reds")
 st.plotly_chart(choropleth, use_container_width=True)
         
-#top 6 lowest and highest population 2021
-def compute_top5population(df):
-    df_top5population = df.sort_values(['PopulationCensus2021-11-25'],ascending=False).head(6)[["Name","PopulationCensus2021-11-25"]].rename(columns={'PopulationCensus2021-11-25': 'Population'})
-    return df_top5population
-
-#top 6 lowest and highest population 2021
-def compute_low5population(df):
-    df_low5population = df.sort_values(['PopulationCensus2021-11-25'],ascending=True).head(6)[["Name","PopulationCensus2021-11-25"]].rename(columns={'PopulationCensus2021-11-25': 'Population'})
-    return df_low5population
-
-#generate line chart to show the increase of population by province
-def genlinechart_province():
-    df_province = pd.read_csv("data/province_complete.csv")
-    df_temp = df_province[["Name", "Population1981","Population1991","Population2001","Population2011","Population2021"]]
-    df_temp = df_temp.rename(columns={"Population1981" : 1981,"Population1991" : 1991,"Population2001" : 2001,"Population2011" : 2011,"Population2021":2021})
-    return df_temp.set_index('Name').T
 
 
 st.table(compute_top5population(df_nepal))
 st.table(compute_low5population(df_nepal))
 
-st.line_chart(genlinechart_province())
+st.line_chart(genlinechart_province(df_province))
+st.plotly_chart(genpiechart_province(df_province), use_container_width=True)
